fedded9e366df12b69e0ead13448b27b2bc12da8
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2024-09-23 04:22:13 +0000

    :sparkles: add kafka_setup container

--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic $TOPIC
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server $BROKER_HOST:9092 --create --if-not-exists --topic $TOPIC
